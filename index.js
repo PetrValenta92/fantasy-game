714987a377a914e347f7b10287ae9b8a48663720
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-21 07:50:32 +0200

    Move Wizzard and Ork card html to index.js

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ Challenge:
    JavaScript.
 2. Do the same for Orc card. 
 */
+
+function getHeroHtml() {
+   let heroHtml = '';
+   heroHtml = `
+      <div class="character-card">
+          <h4 class="name">Wizard</h4>
+          <img class="avatar" src="images/wizard.png" />
+          <p class="health">health: <b> 60 </b></p>
+          <div class="dice-container"><div class="dice">6</div></div>
+      </div>
+   `
+   return heroHtml;
+}
+
+function getMonsterHtml() {
+   let monsterHtml = '';
+   monsterHtml = `
+      <div class="character-card">
+          <h4 class="name">Orc</h4>
+          <img class="avatar" src="images/orc.png" />
+          <p class="health">health: <b> 10 </b></p>
+          <div class="dice-container"><div class="dice">4</div></div>
+      </div>
+   `
+
+   return monsterHtml;
+}
+
+function renderHero() {
+   document.getElementById("hero").innerHTML = getHeroHtml();
+}
+
+function renderMonster() {
+   document.getElementById("monster").innerHTML = getMonsterHtml();
+}
+
+renderHero();
+renderMonster();
