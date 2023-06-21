48148fa061dd6ccfcc3116cd1661f1b5a941b18f
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-21 08:53:36 +0200

    Add object destructuring in renderCharacter()

--- index.js
+++ index.js
@@ -15,16 +15,48 @@ const monster = {
 }
 
 function renderCharacter(character) {
-   
-   document.getElementById(character.elementId).innerHTML = `
+
+   const {elementId, name, avatar, health, diceRoll} = character;
+
+   document.getElementById(elementId).innerHTML = `
       <div class="character-card">
-         <h4 class="name">${character.name}</h4>
-         <img class="avatar" src=${character.avatar} />
-         <p class="health">health: <b>${character.health}</b></p>
-         <div class="dice-container"><div class="dice">${character.diceRoll}</div></div>
+         <h4 class="name">${name}</h4>
+         <img class="avatar" src=${avatar} />
+         <p class="health">health: <b>${health}</b></p>
+         <div class="dice-container"><div class="dice">${diceRoll}</div></div>
       </div>
    `
 }
 
 renderCharacter(hero);
 renderCharacter(monster);
+
+// ----------------- OBJECT DESTRUCTURING ------------------------------
+
+// ------------ Object -----------------
+// const favouriteFilm = {
+//    title: "Top Gun",
+//    year: "1986",
+//    genre: "action",
+//    star: "Tom Cruise",
+//    director: "Tony Scott"
+// } 
+
+// ----------- Possible but not DRY method -----------
+// const title = favouriteFilm.title
+// const year = favouriteFilm.year
+// const genre = favouriteFilm.genre 
+// const star = favouriteFilm.star
+// const director = favouriteFilm.director
+
+// ------------- Destructured object --------------
+// const {title, year, genre, star, director} = favouriteFilm
+
+
+// ------------- Code ------------------
+// console.log(`My favourite film is ${title} 
+// starring ${star}. It's an ${genre} film that 
+// was directed by ${director} and released in ${year}`)
+
+// -------------- Output -------------------
+//My favourite film is Top Gun starring Tom Cruise. It's an action film that was directed by Tony Scott and released in 1986
