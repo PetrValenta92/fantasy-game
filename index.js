b354f41d7fd3c3c2132edacc5846e24bdcf81016
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-22 08:39:35 +0200

    Add diceCount variable and update character html

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
    name: 'Wizard',
    avatar: 'images/wizard.png',
    health: 60,
-   diceRoll: 6
+   diceRoll: 6,
+   diceCount: 3
 }
 
 const monster = {
@@ -11,19 +12,29 @@ const monster = {
    name: 'Ork',
    avatar: 'images/orc.png',
    health: 10,
-   diceRoll: 4
+   diceRoll: 4,
+   diceCount: 1
 }
+
 
 function renderCharacter(character) {
 
-   const {elementId, name, avatar, health, diceRoll} = character;
+   const {elementId, name, avatar, health, diceRoll, diceCount} = character;
+
+   let diceHtml = '';
+
+   for (let i = 0; i < diceCount; i++) {
+      diceHtml += `<div class="dice">6</div>`
+   }
 
    document.getElementById(elementId).innerHTML = `
       <div class="character-card">
          <h4 class="name">${name}</h4>
          <img class="avatar" src=${avatar} />
          <p class="health">health: <b>${health}</b></p>
-         <div class="dice-container"><div class="dice">${diceRoll}</div></div>
+         <div class="dice-container">
+            ${diceHtml}
+         </div>
       </div>
    `
 }
