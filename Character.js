21516cff685d3db6dafa532203f094b13770f0e2
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-26 20:04:53 +0200

    Connect data from Character.js with index.js

---
+++ Character.js
@@ -0,0 +1,29 @@
+// Named import must have the same name and curly brackets!!!
+import { getDiceRollArray } from "./utils.js";
+
+function Character(data) {
+    Object.assign(this, data);
+ 
+    this.getCharacterHtml = function() {
+       const { elementId, name, avatar, health, diceCount } = this;
+       const diceHtml = this.getDiceHtml(diceCount);
+       
+       return `
+          <div class="character-card">
+             <h4 class="name"> ${name} </h4>
+             <img class="avatar" src="${avatar}" />
+             <div class="health">health: <b> ${health} </b></div>
+             <div class="dice-container">    
+                ${diceHtml}
+             </div>
+          </div>`;
+    }
+ 
+    this.getDiceHtml = function(diceCount) {
+       return getDiceRollArray(diceCount).map( function (num) {
+          return `<div class="dice">${num}</div>`;   
+       }).join('');
+    }
+ }
+
+ export default Character;
