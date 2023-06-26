93bc083afc6f8afe2ca9134f323c21164459ae1e
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-26 19:21:21 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,19 @@
+const characterData = {
+
+    hero: {
+       elementId: 'hero',
+       name: 'Wizard',
+       avatar: 'images/wizard.png',
+       health: 60,
+       diceCount: 3
+    },
+    
+    monster: {
+       elementId: 'monster',
+       name: 'Ork',
+       avatar: 'images/orc.png',
+       health: 10,
+       diceCount: 1
+    }
+ }
+ 
