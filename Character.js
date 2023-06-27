fe95d1ed72105e712b85294383a0ce8914e449f3
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-27 08:54:45 +0200

    Update Character.js

--- Character.js
+++ Character.js
@@ -2,19 +2,22 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
 function Character(data) {
+   // Take data from "data.js" and convert it to "this" constructor function
     Object.assign(this, data);
 
+    // Add data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
     this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
-   //  this.getDiceHtml = function(diceCount) {
-   //     return getDiceRollArray(diceCount).map( function (num) {
-   //        return `<div class="dice">${num}</div>`;   
-   //     }).join('');
-   //  }
+    this.currentDiceScore = new Array();
+
+    this.getDiceHtml = function(diceCount) {
+       return getDiceRollArray(diceCount).map( function (num) {
+          return `<div class="dice">${num}</div>`;   
+       }).join('');
+    }
  
     this.getCharacterHtml = function() {
-       const { name, avatar, health, diceCount } = this;
-       // const diceHtml = this.getDiceHtml(diceCount);
+       const { name, avatar, health, diceCount, diceArray } = this;       
        
        return `
           <div class="character-card">
@@ -22,7 +25,7 @@ function Character(data) {
              <img class="avatar" src="${avatar}" />
              <div class="health">health: <b> ${health} </b></div>
              <div class="dice-container">    
-                ${this.diceArray}
+                ${diceArray}
              </div>
           </div>`;
     }
