dba7bed6565b14ee8bddf17e6ea1826047fe8c34
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-28 07:22:11 +0200

    Add method to render diceHtml after btn is clicked

--- Character.js
+++ Character.js
@@ -5,15 +5,18 @@ function Character(data) {
    // Take data from "data.js" and convert it to "this" constructor function
     Object.assign(this, data);
 
-    // Add data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
+    // Take data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
     this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
-    this.currentDiceScore = new Array();
+    // Take data from currentDiceCount array and add it into this.diceArray property
+    // Return a string of each dice HTML elements
+    this.getDiceHtml = function() {
+      // Fill currentDiceCount array with random numbers from getDiceRollArray()
+       this.currentDiceCount = getDiceRollArray(this.diceCount);
 
-    this.getDiceHtml = function(diceCount) {
-       return getDiceRollArray(diceCount).map( function (num) {
-          return `<div class="dice">${num}</div>`;   
-       }).join('');
+       this.diceArray = this.currentDiceCount.map(function(num) {
+            return `<div class="dice">${num}</div>`;
+         }).join('');
     }
  
     this.getCharacterHtml = function() {
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import charData from "./data.js";
 import Character from "./Character.js";
 
 function attack() {
+   wizard.getDiceHtml();
+   ork.getDiceHtml();
    render();
 }
 
