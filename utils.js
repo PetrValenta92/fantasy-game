94bce9ceabe50014921ae7d781ae6a06bd1661d4
Petr Valenta <petr.valenta.92@gmail.com>
2023-06-28 09:13:58 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
 // Place where to store functions that are used multiple times = utility functions
+
+
 function getDiceRollArray(diceCount) {
     return new Array(diceCount).fill(0).map(function() {
        return Math.floor(Math.random() * 6) + 1;
