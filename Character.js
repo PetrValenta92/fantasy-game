b902cf420944e613a8dcdffc060c1dfea137f10f
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-10 08:53:53 +0200

    Add health bar method

--- Character.js
+++ Character.js
@@ -1,6 +1,9 @@
 // Named import must have the same name and curly brackets!!!
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
+const getPercentage = (remainingHealt, maximumHealt) => 
+   (100 * remainingHealt) / maximumHealt;
+
 function Character(data) {
    // Take data from "data.js" and convert it to "this" constructor function
     Object.assign(this, data);
@@ -18,6 +21,9 @@ function Character(data) {
          `<div class="dice">${num}</div>` ).join('');
     }
 
+    // The value of this.maxHealth is set at the beginning and doesn't change
+    this.maxHealth = this.health;
+
     this.takeDamage = function (attackScoreArray) {
 
       const totalAttackScore = attackScoreArray.reduce( (total, score) =>  total + score );
@@ -29,9 +35,15 @@ function Character(data) {
          this.dead = true;
       } 
     }
+
+    this.getHealthBarHtml = function() {
+      const percent = getPercentage(this.health, this.maxHealth);
+      console.log(percent);
+    }
     
     this.getCharacterHtml = function() {
-       const { name, avatar, health, diceCount, diceArray } = this;       
+       const { name, avatar, health, diceCount, diceArray } = this;
+       const healthBar = this.getHealthBarHtml();       
        
        return `
           <div class="character-card">
