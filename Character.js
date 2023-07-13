f39e9456486c4f72173d421d0fa80ba87b7ee33d
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-13 09:09:07 +0200

    Refactor Character Constructor to Character Class

--- Character.js
+++ Character.js
@@ -1,30 +1,32 @@
 // Named import must have the same name and curly brackets!!!
 import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from "./utils.js";
 
-function Character(data) {
-   // Take data from "data.js" and convert it to "this" constructor function
-    Object.assign(this, data);
+// ------------------------------------- CLASS --------------------------------------------
+class Character {
+   
+   constructor(data) {
+      // Take data from "data.js" and convert it to "this" constructor function
+      Object.assign(this, data);
 
-    // Take data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
-    this.diceHtml = getDicePlaceholderHtml(this.diceCount);
+      // Take data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
+      this.diceHtml = getDicePlaceholderHtml(this.diceCount);
 
-    // Take data from currentDiceScore array and add it into this.diceHtml property
-    // Return a this.DiceHtml = string of each dice HTML elements
-    this.setDiceHtml = function() {
+      // The value of this.maxHealth is set at the beginning and doesn't change
+      this.maxHealth = this.health;
+   }
+   
+   // Take data from currentDiceScore array and add it into this.diceHtml property
+   // Return a this.DiceHtml = string of each dice HTML elements
+   setDiceHtml() {
 
       // Fill currentDiceScore array with random numbers from getDiceRollArray() (depending on diceCount)
-       this.currentDiceScore = getDiceRollArray(this.diceCount);
-       this.diceHtml = this.currentDiceScore.map( num => 
+      this.currentDiceScore = getDiceRollArray(this.diceCount);
+      this.diceHtml = this.currentDiceScore.map( num => 
          `<div class="dice">${num}</div>` ).join('');
     }
 
-    // The value of this.maxHealth is set at the beginning and doesn't change
-    this.maxHealth = this.health;
-
-    this.takeDamage = function (attackScoreArray) {
-
+   takeDamage(attackScoreArray) {
       const totalAttackScore = attackScoreArray.reduce( (total, score) =>  total + score );
-      
       this.health -= totalAttackScore;
 
       if (this.health <= 0) {
@@ -33,9 +35,8 @@ function Character(data) {
       } 
     }
 
-    this.getHealthBarHtml = function() {
+   getHealthBarHtml() {
       const percent = getPercentage(this.health, this.maxHealth);
-      
       return `
          <div class="health-bar-outer">
             <div class="health-bar-inner ${percent <= 25 ? "danger" : ""}" 
@@ -45,7 +46,7 @@ function Character(data) {
       `
     }
     
-    this.getCharacterHtml = function() {
+   getCharacterHtml() {
        const { name, avatar, health, diceCount, diceHtml } = this;
        const healthBar = this.getHealthBarHtml();       
        
@@ -60,6 +61,68 @@ function Character(data) {
              </div>
           </div>`;
     }
- }
+}
 
- export default Character;
+export default Character;
+
+// -------------------------------- CONSTRUCTOR FUNCTION ----------------------------------------
+// function Character(data) {
+//    // Take data from "data.js" and convert it to "this" constructor function
+//     Object.assign(this, data);
+
+//     // Take data from getDicePlaceholderHtml() and add it into the "this" BUT NOT into "data"
+//     this.diceHtml = getDicePlaceholderHtml(this.diceCount);
+
+//     // Take data from currentDiceScore array and add it into this.diceHtml property
+//     // Return a this.DiceHtml = string of each dice HTML elements
+//     this.setDiceHtml = function() {
+
+//       // Fill currentDiceScore array with random numbers from getDiceRollArray() (depending on diceCount)
+//        this.currentDiceScore = getDiceRollArray(this.diceCount);
+//        this.diceHtml = this.currentDiceScore.map( num => 
+//          `<div class="dice">${num}</div>` ).join('');
+//     }
+
+//     // The value of this.maxHealth is set at the beginning and doesn't change
+//     this.maxHealth = this.health;
+
+//     this.takeDamage = function (attackScoreArray) {
+
+//       const totalAttackScore = attackScoreArray.reduce( (total, score) =>  total + score );
+      
+//       this.health -= totalAttackScore;
+
+//       if (this.health <= 0) {
+//          this.health = 0;
+//          this.dead = true;
+//       } 
+//     }
+
+//     this.getHealthBarHtml = function() {
+//       const percent = getPercentage(this.health, this.maxHealth);
+      
+//       return `
+//          <div class="health-bar-outer">
+//             <div class="health-bar-inner ${percent <= 25 ? "danger" : ""}" 
+//                style="width: ${percent}%;">
+//             </div>
+//          </div>
+//       `
+//     }
+    
+//     this.getCharacterHtml = function() {
+//        const { name, avatar, health, diceCount, diceHtml } = this;
+//        const healthBar = this.getHealthBarHtml();       
+       
+//        return `
+//           <div class="character-card">
+//              <h4 class="name"> ${name} </h4>
+//              <img class="avatar" src="${avatar}" />
+//              <div class="health">health: <b> ${health} </b></div>
+//              ${healthBar}
+//              <div class="dice-container">    
+//                 ${diceHtml}
+//              </div>
+//           </div>`;
+//     }
+//  }
